1eabd315d6d1520b36cf346a0d9bc3688ffc5bec
ponopono0322 <32767165+ponopono0322@users.noreply.github.com>
2020-10-31 18:48:28 +0900

    camera2 model adapted.

---
+++ app/src/main/java/com/example/cardeep/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.cardeep;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ImageButton imageButton = (ImageButton) findViewById(R.id.CameraStartButton);
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), CameraActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
